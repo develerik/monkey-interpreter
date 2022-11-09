089e30b312ff463fb3cf16178f857358ecd93d39
Erik Bender <erik.bender@develerik.dev>
2022-11-09 19:23:33 +0100

    add token type

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string // performance can be improved by using 'int' or 'byte' (p.12)
+
+type Token struct { // can later be improved by attaching filenames and line numbers (p.13)
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
