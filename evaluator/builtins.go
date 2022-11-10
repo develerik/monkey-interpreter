30dd29aac73116c42097ae1972bfd8dd40dc39bc
Erik Bender <erik.bender@develerik.dev>
2022-11-10 19:02:01 +0100

    add puts() builtin function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/develerik/monkey-interpreter/object"
+import (
+	"fmt"
+
+	"github.com/develerik/monkey-interpreter/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -109,6 +113,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
