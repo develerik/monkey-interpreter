425fa89943bcaaed90bde910d6e8cf9ba3c2d1b4
Erik Bender <erik.bender@develerik.dev>
2022-11-10 17:25:24 +0100

    add evaluation function calls

--- object/object.go
+++ object/object.go
@@ -1,6 +1,12 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/develerik/monkey-interpreter/ast"
+)
 
 type ObjectType string
 
@@ -27,8 +33,10 @@ type Error struct {
 	Message string
 }
 
-type Environment struct {
-	store map[string]Object
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
 }
 
 const (
@@ -37,6 +45,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
@@ -54,17 +63,21 @@ func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
-	return &Environment{store: s}
-}
-func (e *Environment) Get(name string) (Object, bool) {
-	obj, ok := e.store[name]
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
 
-	return obj, ok
-}
-func (e *Environment) Set(name string, val Object) Object {
-	e.store[name] = val
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
 
-	return val
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
 }
